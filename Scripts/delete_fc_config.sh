cc3905c205b0d3c9df5d7c1501e9ca9c474ea52a
howie-j <82042580+howie-j@users.noreply.github.com>
2023-04-08 12:31:06 +0200

    Initial commit

---
+++ Scripts/delete_fc_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Delete all FreeCAD config folders and files
+
+rm -rfv ~/.local/share/FreeCAD
+rm -rfv ~/.config/FreeCAD
+rm -rfv ~/.cache/FreeCAD
+rm -rfv ~/.cache/qtshadercache-x86_64-little_endian-lp64
+rm -rfv ~/.cache/mesa_shader_cache
+rm -rfv ~/.cache/qt_compose_cache_little_endian_*
---
+++ Scripts/install_addons.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone https://github.com/Zolko-123/FreeCAD_Assembly4.git ~/.local/share/FreeCAD/Mod/Assembly4
+git clone https://github.com/DanMiel/QuickMeasure.git ~/.local/share/FreeCAD/Mod/QuickMeasure
+
+
